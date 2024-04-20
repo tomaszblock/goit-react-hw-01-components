0cb99eb894c5878dc89bbd3683b1f27015c6a27c
tomaszblock <127046228+tomaszblock@users.noreply.github.com>
2024-04-20 15:28:18 +0200

    done

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,34 +1,34 @@
-import data from '../json/transactions.json'
-import css from "../css/Transactions.module.css";
-
+import data from '../json/transactions.json';
+import css from '../css/Transactions.module.css';
 
 const Transaction = ({ props }) => {
   return (
-<tbody>
-{props.map((prop, index) => (
-    <tr key={prop.id} className={index % 2 === 1 ? css.dupa : css.dupa2}>
-    <td>{prop.type}</td>
-    <td>{prop.amount}</td>
-    <td>{prop.currency}</td>
-  </tr>
-  ))}
-</tbody>
+    <tbody>
+      {props.map((prop, index) => (
+        <tr
+          key={prop.id}
+          className={`${index % 2 === 1 ? css.odd : css.even} ${css.test}`}
+        >
+          <td className={css.type}>{prop.type}</td>
+          <td>{prop.amount}</td>
+          <td>{prop.currency}</td>
+        </tr>
+      ))}
+    </tbody>
   );
 };
 
 export const Transactions = () => {
   return (
-<table className={css.transactionHistory}>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-  </thead>
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr className={css.topic}>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
       <Transaction props={data} />
-      </table>
+    </table>
   );
 };
-
-
